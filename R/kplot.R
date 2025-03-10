143cadffaad905cb6a9f36f30ad1d6b92729388c
Kevin Bosirany ORLANDO <kevinbosirany@gmail.com>
2025-03-10 15:10:01 +0100

    feat: Add kplot function for generic plotting with method dispatch

---
+++ R/kplot.R
@@ -0,0 +1,25 @@
+#' kplot Function
+#'
+#' This function is a generic method for plotting objects. It uses the `UseMethod` function to dispatch the appropriate method based on the class of the object `x`.
+#'
+#' @param x An object to be plotted.
+#' @param ... Additional arguments to be passed to the plotting method.
+#'
+#' @return A plot of the object `x`.
+#' 
+#' @export
+#'
+#' @examples
+#' kplot(1:10)
+#' kplot.default(1:10)
+#' 
+kplot <- function(x, ...) {
+  UseMethod("kplot", x)
+}
+
+#' @export
+#' @rdname kplot
+#' 
+kplot.default <- function(x, ...) {
+  plot(x, ...)
+}
